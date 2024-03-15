e25f0cc91c4d62fa0d98cd04a1743f1ab6049e57
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-15 03:29:10 +0200

    *

--- src/vm/eva_vm.rs
+++ src/vm/eva_vm.rs
@@ -1,20 +1,44 @@
 use crate::bytecode::code;
+use crate::value::EvaValue;
 use crate::DIE;
-pub struct EvaVM {
-    code: Vec<u8>,
-    ip: usize, // instruction pointer (aka program counter)
+use std::mem;
+
+pub struct EvaVM<'a> {
+    code: Vec<usize>,
+    constants: Vec<EvaValue>,
+    stack: [EvaValue; 512],
+    ip: usize,                // instruction pointer (aka program counter)
+    sp: Option<&'a EvaValue>, // stack ponter
 }
-impl EvaVM {
-    pub fn new() -> EvaVM {
+
+impl EvaVM<'static> {
+    pub fn new() -> EvaVM<'static> {
         EvaVM {
-            code: Vec::new(),
+            code: vec![],
+            constants: vec![],
+            stack: unsafe { mem::zeroed() },
             ip: 0,
+            sp: None,
         }
+    }
+
+    fn push(&mut self, value: &'static EvaValue) {
+        match self.sp {
+            Some(pointer) => {
+                if pointer - &self.stack[0] == 512 {
+                    DIE!("Stack limit")
+                }
+            }
+            _ => (),
+        };
+
+        self.sp += 1
     }
 
     pub fn exec(&mut self, program: String) {
         let _ = program;
-        self.code = vec![code::OPERATION_HALT];
+        self.constants.push(EvaValue::new_number(42));
+        self.code = vec![code::OPERATION_CONST, 0, code::OPERATION_HALT];
         self.eval()
     }
 
@@ -27,6 +51,11 @@ impl EvaVM {
                     println!("HALT");
                     return;
                 }
+                code::OPERATION_CONST => {
+                    let index = self.read_byte();
+                    let constValue = &self.constants[index];
+                    // push(constValue)
+                }
                 _ => {
                     // DIE!("Unknown opcode: {}", opcode);
                     DIE!("Unknown opcode: {}", opcode);
@@ -35,8 +64,8 @@ impl EvaVM {
         }
     }
 
-    fn read_byte(&mut self) -> u8 {
-        let byte: u8 = self.code[self.ip];
+    fn read_byte(&mut self) -> usize {
+        let byte = self.code[self.ip] as usize;
         self.ip += 1;
         byte
     }
