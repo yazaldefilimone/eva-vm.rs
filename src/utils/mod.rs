0f8e21133d5fc7874e350119a11087f19ee1c5a0
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-06 01:21:53 +0200

    *

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,9 @@
+use crate::node::node::Node;
+
 pub const STACK_LIMIT: usize = 512; // Maximum number of items on the stack
+
+pub fn get_node_value(node: Node) -> i32 {
+  match node {
+    Node::Number(n) => n,
+  }
+}
