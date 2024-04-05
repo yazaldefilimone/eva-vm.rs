af7e0b294e1cb6239028a20efbf36bdd4f79fd97
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-06 01:20:14 +0200

    *

--- src/bytecode/code.rs
+++ src/bytecode/code.rs
@@ -1,2 +1,6 @@
-pub const OPERATION_HALT: usize = 0x00; // Halts the program
-pub const OPERATION_CONST: usize = 0x01; // Pushes a constant onto the stack
+// halt = stop the program
+pub const HALT: usize = 0x00;
+// const = push a constant onto the stack
+pub const CONST: usize = 0x01;
+// add = add two values together
+pub const ADD: usize = 0x02;
